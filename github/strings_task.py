a815eaa9324f340fe8665c7159a204885f597ce2
Niharika-2003-kavati <kavatineharika1245@gmail.com>
2026-02-26 07:47:24 +0530

    pract

---
+++ github/strings_task.py
@@ -0,0 +1,20 @@
+#removing space from the string
+#username ="niharika 123"
+#sername.strip()
+#print(username)
+
+#finding substrings from the given string
+#str1 ="PYTHON is Easy"
+#op=str1.find('E')
+#print(op)
+
+#counting substring in given string
+#str1="PYTHON PYTHON JAVA JS PYTHON"
+#op=str1.count('th')
+#print(op)
+
+#splitting the string
+str1 ="DOSOMETHINGGREAT"
+op=str1.split()
+print(op)
+
